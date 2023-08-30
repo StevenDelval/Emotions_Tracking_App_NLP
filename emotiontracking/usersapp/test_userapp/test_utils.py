7b63566e5df3b331517aa387c80d402c6133fc30
StevenDelval <asteven59220@free.fr>
2023-08-30 10:09:53 +0200

    feat: move test

--- emotiontracking/usersapp/test_userapp/test_utils.py
+++ emotiontracking/usersapp/test_userapp/test_utils.py
@@ -3,11 +3,3 @@ from django.urls import reverse
 from django.http import HttpRequest
 from django.shortcuts import render
 from usersapp.views import home
-
-class HomeViewTestCase(TestCase):
-    def test_home_view(self):
-        request = HttpRequest()
-        response = home(request)
-        
-        self.assertEqual(response.status_code, 200)  # Vérifie que la réponse est un succès (code 200)
-        self.assertTemplateUsed(response, 'usersapp/home.html') 
